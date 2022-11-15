0ef49b3833661aab74a778605034f8ee044cead0
Kay <klarson@quantummetric.com>
2022-11-14 17:54:11 -0700

    create swift app

---
+++ To-Do/ToDo/ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Kayla Larson on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
